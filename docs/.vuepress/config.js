92570b3a56092253cf456e72b6566f76525b5fa3
ibook86 <zyh15637540388@gmail.com>
2021-08-22 23:35:21 +0800

    update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,11 @@
 // .vuepress/config.js
-const { config } = require("vuepress-theme-hope");
+const {config} = require("vuepress-theme-hope");
 
 module.exports = config({
     // your config here
+
+    themeConfig: {
+        logo: "/logo.svg",
+        hostname: "https://vuepress-pi.vercel.app",
+    }
 });
