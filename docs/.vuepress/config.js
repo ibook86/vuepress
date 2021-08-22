6bdeba93de850f578e9c3f7d83e655f7cce0f6c8
ibook86 <zyh15637540388@gmail.com>
2021-08-22 23:39:56 +0800

    add logo

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,7 +3,6 @@ const {config} = require("vuepress-theme-hope");
 
 module.exports = config({
     // your config here
-
     themeConfig: {
         logo: "/logo.svg",
         hostname: "https://vuepress-pi.vercel.app",
